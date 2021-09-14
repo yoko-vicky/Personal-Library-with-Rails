6a88e974829e0c2d5c53b83befd02bb3fa6b773b
yocosaka <sakayoco55@gmail.com>
2021-09-14 14:54:15 +0800

    Updated: database to use postgresql.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_14_064754) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string "name"
     t.string "file"
