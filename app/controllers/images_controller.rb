811bb7b1aee8fe2f1355fcb23e81b109b28994cf
yocosaka <sakayoco55@gmail.com>
2021-11-11 09:10:29 +0900

    Update images controller and Fix image form to display current image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -20,48 +20,45 @@ class ImagesController < ApplicationController
   end
 
   # GET /images/1/edit
-  def edit; end
+  def edit
+    @image.file.cache! unless @image.file.blank?
+  end
 
   # POST /images or /images.json
   def create
     @image = Image.new(image_params)
 
-    respond_to do |format|
-      if @image.save
-        format.html { redirect_to @image, notice: 'Image was successfully created.' }
-        format.json { render :show, status: :created, location: @image }
+    if @image.save
+      flash[:notice] = 'Image was successfully created.'
+      redirect_to image_path(@image)
       # Tagging
       # 0. Retreive URL of the image file in S3: @image.file
       # 1. Based on the image file url of the S3, send a request to Clarifi, retrive taggings as an array
       # 2. Create each Tag based on taggings array
       # 3. Make a connection by posting ImageTag row to ImageTag through table
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
+    else
+      # @image.errors
+      flash[:alert] = 'Something is wrong'
+      render 'new'
     end
   end
 
   # PATCH/PUT /images/1 or /images/1.json
   def update
-    respond_to do |format|
-      if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-        format.json { render :show, status: :ok, location: @image }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
+    if @image.update(image_params)
+      flash[:notice] = 'Image was successfully updated.'
+      redirect_to image_path(@image)
+    else
+      flash[:alert] = 'Something is wrong'
+      render 'edit'
     end
   end
 
   # DELETE /images/1 or /images/1.json
   def destroy
     @image.destroy
-    respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Image was successfully deleted.'
+    redirect_to root_path
   end
 
   private
@@ -73,6 +70,6 @@ class ImagesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def image_params
-    params.require(:image).permit(:name, :file)
+    params.require(:image).permit(:name, :file, :file_cache)
   end
 end
--- app/views/images/_image.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! image, :id, :name, :file, :description, :created_at, :updated_at
-json.url image_url(image, format: :json)
